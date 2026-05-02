f3543d7dde19aed663fbfdf991cee0c384ea50a8
JoacinB <joaquinborgesborio@gmail.com>
2026-05-02 13:54:00 -0300

    RED: ParserRegistry should resolve a registered code to the configured parser instance

---
+++ tests/Unit/Importing/ParserRegistryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Importing;
+
+use App\Importing\Contracts\SupplierParser;
+use App\Importing\Exceptions\ParserNotFoundException;
+use App\Importing\ParserRegistry;
+use Tests\TestCase;
+
+final class ParserRegistryTest extends TestCase
+{
+    public function test_resolves_a_registered_code_to_the_configured_parser_instance(): void
+    {
+        config()->set('importing.parsers', [
+            'fake' => FakeParser::class,
+        ]);
+
+        $registry = $this->app->make(ParserRegistry::class);
+
+        $this->assertInstanceOf(FakeParser::class, $registry->resolve('fake'));
+    }
+}
+
+final class FakeParser implements SupplierParser
+{
+    public function parse(string $filePath): iterable
+    {
+        return [];
+    }
+}
